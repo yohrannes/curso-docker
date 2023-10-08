b265ca794ab4246292ed4c933a9bd539eb88e7fe
yohrannes <51239326+yohrannes@users.noreply.github.com>
2023-10-08 06:13:51 -0300

    updating script

--- 4_yaml/app.py
+++ 4_yaml/app.py
@@ -2,7 +2,7 @@ import yaml
 
 if __name__ == '__main__':
 
-  stream = open("test.yaml", "r")
+  stream = open("teste2.yaml", "r")
   dictionary = yaml.safe_load(stream)
 
   for key, value in dictionary.items():
